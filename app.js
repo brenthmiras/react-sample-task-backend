5882a099f55ed68221520f865772f62a9dbbebc4
Brenth Andrew J Miras <brenth.miras@solarsuite.com.au>
2019-10-26 22:25:18 +0800

    Base code

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('axios');
+const cors = require('cors');
+
+// Initialize app
+const app = express();
+const port = 3000;
+
+// Allow all cross origin requests
+app.use(cors());
+
+app.get('/contacts', (req, res) => {
+
+    const organization_id = '649249007';
+
+    const authtoken = 'db36e02a50b57e081efe533a8a0f834b';
+
+    const url = `https://books.zoho.com/api/v3/contacts?organization_id=${organization_id}`;
+
+    const options = {
+        headers: {
+            'Authorization': `Zoho-authtoken ${authtoken}`
+        }
+    }
+
+    // Fire http request to zoho api
+    http.get(url, options)
+    .then( response => {
+        res.status(200).send(response.data);
+    })
+    .catch( err => {
+        res.status(500).send('Problem fetching data.');
+    });
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
